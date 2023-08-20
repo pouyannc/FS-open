fdcb88c811fc51f28a65508bb18b119d7d392d59
Pouyan <pouyannc@gmail.com>
2023-08-20 18:48:01 -0400

    Complete part 1.2, Refactor Content component

--- part1/courseinfo_app/src/App.jsx
+++ part1/courseinfo_app/src/App.jsx
@@ -2,19 +2,21 @@ const Header = ({ course }) => {
   return <h1>{course}</h1>
 }
 
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  )
+}
+
 const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
   return (
-    <>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-    </>
+    <div>
+      <Part name={part1} exercises={exercises1} />
+      <Part name={part2} exercises={exercises2} />
+      <Part name={part3} exercises={exercises3} />
+    </div>
   )
 }
 
