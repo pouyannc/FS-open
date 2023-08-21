bbcac66b2f0c8178d0820d9ced94e06ec1bf2009
Pouyan <pouyannc@gmail.com>
2023-08-21 16:07:31 -0400

    Complete unicafe app

--- part1/unicafe/README.md
+++ part1/unicafe/README.md
@@ -1,8 +1,5 @@
-# React + Vite
+# Unicafe feedback app
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This code is written for completion of FullStack Open exercises part 1.6-1.11. This is a feedback app that updates state after button clicks, and displays state and other stats on the page.
 
-Currently, two official plugins are available:
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,9 +8,33 @@ const Button = ({ handleClick, text }) => {
 
 const Stat = ({ rating, count }) => {
   return (
-    <div>
-      {rating} {count}
-    </div>
+    <tr>
+      <td>{rating}</td>
+      <td>{count}</td>
+    </tr>
+    
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+
+  return (
+    <>
+      <h1>Statistics</h1>
+      {all > 0 
+        ? <table>
+            <tbody>
+              <Stat rating="good" count={good} />
+              <Stat rating="neutral" count={neutral} />
+              <Stat rating="bad" count={bad} />
+              <Stat rating="all" count={all} />
+              <Stat rating="average" count={(good - bad) / all} />
+              <Stat rating="positive" count={good / all * 100 + "%"} />
+            </tbody>
+          </table>
+        : <div>No feedback given</div>}
+    </>
   )
 }
 
@@ -18,8 +42,6 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const all = good + neutral + bad;
 
   const handleClick = (setRating, rating) => {
     setRating(rating + 1);
@@ -34,13 +56,7 @@ function App() {
         <Button handleClick={() => handleClick(setBad, bad)} text="bad" />
       </span>
 
-      <h1>Statistics</h1>
-      <Stat rating="good" count={good} />
-      <Stat rating="neutral" count={neutral} />
-      <Stat rating="bad" count={bad} />
-      <Stat rating="all" count={all} />
-      <Stat rating="average" count={(good - bad) / all} />
-      <Stat rating="positive" count={good / all} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
