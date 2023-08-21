c138359299373df8358cd40bc1b98795c0d6d60d
Pouyan <pouyannc@gmail.com>
2023-08-21 15:31:48 -0400

    Unicafe v1, part 1.6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+const Stat = ({ rating, count }) => {
+  return (
+    <div>
+      {rating} {count}
+    </div>
+  )
+}
+
+function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClick = (setRating, rating) => {
+    setRating(rating + 1);
+  };
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <span>
+        <Button handleClick={() => handleClick(setGood, good)} text="good" />
+        <Button handleClick={() => handleClick(setNeutral, neutral)} text="neutral" />
+        <Button handleClick={() => handleClick(setBad, bad)} text="bad" />
+      </span>
+
+      <h1>Statistics</h1>
+      <Stat rating="good" count={good} />
+      <Stat rating="neutral" count={neutral} />
+      <Stat rating="bad" count={bad} />
+    </div>
+  )
+}
+
+export default App
