822e0d7923de96d42074a90f0a51a7778c655737
Pouyan <pouyannc@gmail.com>
2023-08-22 16:23:50 -0400

    Fetch data from json server, part 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Filter = ({ filter, handleChange }) => {
   return (
@@ -32,12 +33,7 @@ const Persons = ({ persons, filter }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
@@ -62,6 +58,14 @@ const App = () => {
       setPersons(persons.concat({name: newName, number: newNumber, id: persons[persons.length-1].id+1}));
     }
   }
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(res => {
+        setPersons(res.data);
+      })
+  }, [])
 
   return (
     <div>
