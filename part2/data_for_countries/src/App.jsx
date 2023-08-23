b21ad908fff8592307f6efdf0a7815a9528c6538
Pouyan <pouyannc@gmail.com>
2023-08-23 04:12:02 -0400

    Dynamic search for country data

--- part2/data_for_countries/src/App.jsx
+++ part2/data_for_countries/src/App.jsx
@@ -1,8 +1,64 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
-function App() {
+const Search = ({ input, handleChange }) => {
   return (
-    <div></div>
+    <div>find countries <input value={input} onChange={handleChange} /></div>
+  )
+}
+
+const Country = ({ country }) => {
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+      <div>Capital: {country.capital[0]}</div>
+      <div>Population: {country.population}</div>
+      <h3>Languages:</h3>
+      <ul>
+        {Object.values(country.languages).map((l, i) => <li key={i}>{l}</li>)}
+      </ul>
+      <img style={{width: 200}} src={country.flags.svg} alt={country.flags.alt} />
+    </div>
+  )
+}
+
+const Countries = ({ countries }) => {
+  if (countries && countries.length === 0) return <div>Too many matches, specify another filter</div>
+  else if (countries && countries.length === 1) return <Country country={countries[0]} />
+
+  return (
+    countries &&
+    countries.map((country, i) => {
+      return <div key={i}>{country.name.common}</div>
+    })
+  )
+}
+
+const App = () => {
+  const [searchInput, setSearchInput] = useState('');
+  const [countries, setCountries] = useState(null);
+
+  const handleSearchChange = (e) => {
+    setSearchInput(e.target.value)
+  }
+
+  useEffect(() => {
+    if (searchInput) {
+      axios
+        .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+        .then((res) => {
+          const query = res.data.filter((country) => country.name.common.toLowerCase().includes(searchInput.toLowerCase()))
+          if (query.length <= 10) setCountries(query);
+          else setCountries([]);
+        })
+    } else setCountries(null);
+  }, [searchInput])
+
+  return (
+    <div>
+      <Search input={searchInput} handleChange={handleSearchChange} />
+      {<Countries countries={countries} />}
+    </div>
   )
 }
 
